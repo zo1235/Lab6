d50440106c4bb706b3e4dac1bd5fa9bf23b68c63
Zoher-Dharnojwala <dharnojwala.z@northeastern.edu>
2024-03-23 23:30:12 -0400

    init commit lab session 6

---
+++ Documents/NetBeansProjects/Lab6/src/DatabaseConnector.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+/**
+ * Database Connector class for interacting with database
+ * @author akshatr
+ */
+public class DatabaseConnector {
+
+    private static final String URL = "jdbc:mysql://localhost:1234/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
+    
+  
+    /**
+     * Privatized constructor so as to not allow object creation
+     */
+    private DatabaseConnector() {
+    }
+
+    /**
+     * Insert given user to database
+     * @see User
+     * @param user User object to be added
+     */
+    public static void addUser(User user) {
+        //add to database
+        String query = "INSERT INTO USER(NAME,AGE) VALUES(?,?)";
+        try  (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, user.getName());
+            stmt.setInt(2, user.getAge());
+            int rows = stmt.executeUpdate();
+            System.out.println("Rows impacted : " + rows);
+//            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Return lost of all users in database
+     * @see User
+     * @return list of users
+     */
+    public static ArrayList<User> getAllusers() {
+//        return list of users from db
+        ArrayList<User> users = new ArrayList<>();
+
+        String query = "SELECT * FROM USER";
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                User u = new User();
+                u.setName(rs.getString("Name"));
+                u.setAge(rs.getInt("Age"));
+                u.setId(rs.getInt("Id"));
+                users.add(u);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    /**
+     * Delete given user from database
+     * @see User
+     * @param u User to be deleted
+     * 
+     */
+    public static void deleteUser(User u) {
+        String query = "delete from USER where id = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, u.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Edit given user details in the database
+     * @param oldUser existing user in database
+     * @param newUser modified user details to be added
+     */
+    public static void editUser(User oldUser, User newUser) {
+        String query = "UPDATE USER SET name=?, age=? WHERE id=?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, newUser.getName());
+            stmt.setInt(2, newUser.getAge());
+            stmt.setInt(3, oldUser.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Documents/NetBeansProjects/Lab6/src/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Zoher
+ */
+public class User {
+    private String Name;
+    private int Age;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+    
+    
+
+}
